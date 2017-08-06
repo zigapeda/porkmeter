75b33baef84312f714e89c137f7f53fc1c462f2a
Daniel Elstner <daniel.elstner@gmail.com>
2017-08-06 20:59:07 +0200

    move into push folder; add icon, manifest and fmc sw

---
+++ server/push/service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  // TODO: update title & options using event.data.text() (or whatever the server returns)
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    // TODO: does every event include notification.data.url by default?
+    clients.openWindow(event.notification.data.url)
+  );
+});
