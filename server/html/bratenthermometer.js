0eef67c92926a1ef87ee287af76fb3918d55ae19
Daniel Elstner <daniel.elstner@gmail.com>
2017-08-07 20:31:38 +0200

    enable + disable funktion eingebaut

--- server/html/bratenthermometer.js
+++ server/html/bratenthermometer.js
@@ -1,3 +1,17 @@
+var swRegistration = null;
+
+var config = {
+    apiKey: "AIzaSyD3TZlq2TIS9C719NhvekAL2LfA-Fsr3h4",
+    authDomain: "porkmeter-b3cf2.firebaseapp.com",
+    databaseURL: "https://porkmeter-b3cf2.firebaseio.com",
+    projectId: "porkmeter-b3cf2",
+    storageBucket: "porkmeter-b3cf2.appspot.com",
+    messagingSenderId: "615966299981"
+};
+
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
 
 function progress(data){
     $("#fleischCards").html("");
@@ -19,39 +33,49 @@ function progress(data){
     }
     }
 }
+
 function getData() {
     $.get("/api/GetTemps", progress);
 }
-$(document).ready(function (){
-    getData();
-    setInterval(getData, 5000);
-});
 
-let swRegistration = null;
-// Initialize Firebase
+function enablePush() {
+    alert("enable");
+    messaging.requestPermission()
+    .then(function() {
+        alert("1");
+        console.log('Notification permission granted.');
+        messaging.getToken()
+        .then(function(currentToken) {
+            alert("2");
+            if (currentToken) {
+                console.log("current token", currentToken)
+                $.get("/api/RegisterKey?key=" + currentToken, function(data) {
+                    alert("3");
+                    if(data.success != "ok") {
+                        console.log("Fehler: " + data.error);
+                    } else {
+                        console.log("Key an den Server uebermittelt");
+                    }
+                });
+            } else {
+                console.log('No Instance ID token available. Request permission to generate one.');
+            }
+        })
+        .catch(function(err) {
+            console.log('An error occurred while retrieving token. ', err);
+        });
+    })
+    .catch(function(err) {
+        console.log('Unable to get permission to notify.', err);
+    });
+}
 
-var config = {
-    apiKey: "AIzaSyD3TZlq2TIS9C719NhvekAL2LfA-Fsr3h4",
-    authDomain: "porkmeter-b3cf2.firebaseapp.com",
-    databaseURL: "https://porkmeter-b3cf2.firebaseio.com",
-    projectId: "porkmeter-b3cf2",
-    storageBucket: "porkmeter-b3cf2.appspot.com",
-    messagingSenderId: "615966299981"
-};
-
-firebase.initializeApp(config);
-
-// Retrieve Firebase Messaging object.
-const messaging = firebase.messaging();
-
-messaging.requestPermission()
-.then(function() {
-    console.log('Notification permission granted.');
+function disablePush() {
     messaging.getToken()
     .then(function(currentToken) {
         if (currentToken) {
             console.log("current token", currentToken)
-            $.get("/api/RegisterKey?key=" + currentToken, function(data) {
+            $.get("/api/RemoveKey?key=" + currentToken, function(data) {
                 if(data.success != "ok") {
                     console.log("Fehler: " + data.error);
                 } else {
@@ -65,7 +89,16 @@ messaging.requestPermission()
     .catch(function(err) {
         console.log('An error occurred while retrieving token. ', err);
     });
-})
-.catch(function(err) {
-    console.log('Unable to get permission to notify.', err);
+}
+
+$(document).ready(function (){
+    getData();
+    setInterval(getData, 5000);
+    $("#pushswitch").change(function() {
+        if($("#pushswitch").is(":checked") == true) {
+            enablePush();
+        } else {
+            disablePush();
+        }
+    });
 });
